7a32223e942aaff82c72372f07ec6c6c1e5c152e
Mike Lindegarde <mlindegarde@shinc.org>
2020-03-13 09:40:14 -0400

    UI clean-up and minor refactoring.

---
+++ src/Misc.BgStats/Application/Renderer.fs
@@ -0,0 +1,102 @@
+namespace Misc.BgStats.Application
+
+open System
+
+open Misc.BgStats.Domain.Models
+
+module Renderer =
+    let getScore (scoreType : ScoreType) (scores : Score list) =
+        (scores |> List.find (fun s -> s.ScoreType = scoreType)).Value
+
+    let displayTopLine (longestName : int) =
+        printf "\u2554"
+        [0..3] |> List.iter (fun _ -> printf "\u2550")
+        printf "\u2564"
+        [0..longestName+1] |> List.iter (fun _ -> printf "\u2550")
+        printf "\u2564"
+        [0..7] |> List.iter (fun _ -> printf "\u2550")
+        printf "\u2564"
+        [0..7] |> List.iter (fun _ -> printf "\u2550")
+        printf "\u2564"
+        [0..7] |> List.iter (fun _ -> printf "\u2550")
+        printf "\u2564"
+        [0..7] |> List.iter (fun _ -> printf "\u2550")
+        printfn "\u2557"
+
+        longestName
+
+    let displayHeaders (longestName : int) =
+        printfn
+            "\u2551  # \u2502 %-*s \u2502      T \u2502      R \u2502      P \u2502      O \u2551"
+            longestName
+            "TITLE"
+
+        longestName
+
+    let displayHeaderLine (longestName : int) =
+        printf "\u2560"
+        [0..3] |> List.iter (fun _ -> printf "\u2550")
+        printf "\u256A"
+        [0..longestName+1] |> List.iter (fun _ -> printf "\u2550")
+        printf "\u256A"
+        [0..7] |> List.iter (fun _ -> printf "\u2550")
+        printf "\u256A"
+        [0..7] |> List.iter (fun _ -> printf "\u2550")
+        printf "\u256A"
+        [0..7] |> List.iter (fun _ -> printf "\u2550")
+        printf "\u256A"
+        [0..7] |> List.iter (fun _ -> printf "\u2550")
+        printfn "\u2563"
+
+    let displayBottomLine (longestName : int) =
+        printf "\u255A"
+        [0..3] |> List.iter (fun _ -> printf "\u2550")
+        printf "\u2567"
+        [0..longestName+1] |> List.iter (fun _ -> printf "\u2550")
+        printf "\u2567"
+        [0..7] |> List.iter (fun _ -> printf "\u2550")
+        printf "\u2567"
+        [0..7] |> List.iter (fun _ -> printf "\u2550")
+        printf "\u2567"
+        [0..7] |> List.iter (fun _ -> printf "\u2550")
+        printf "\u2567"
+        [0..7] |> List.iter (fun _ -> printf "\u2550")
+        printfn "\u255D"
+
+    let displayResults (title : string, limit : int) (rankings : Ranking list) =
+        printf "%s%s%s" Environment.NewLine title Environment.NewLine
+
+        let topN = rankings |> List.take (limit)
+
+        let longestName = 
+            topN 
+            |> List.map (fun r -> r.BoardGame.Name.Length)
+            |> List.max
+
+        longestName
+        |> displayTopLine
+        |> displayHeaders
+        |> displayHeaderLine
+
+        topN
+        |> List.iter (fun r ->
+            printfn 
+                "\u2551%3d \u2502 %-*s \u2502 %6.2f \u2502 %6.2f \u2502 %6.2f \u2502 %6.2f \u2551"
+                r.Position
+                longestName
+                r.BoardGame.Name
+                (r.Evaluation.Scores |> List.sumBy (fun s -> s.Value))
+                (r.Evaluation.Scores |> getScore ScoreType.Rating)
+                (r.Evaluation.Scores |> getScore ScoreType.Plays)
+                (r.Evaluation.Scores |> getScore ScoreType.Ownership))
+
+        longestName |> displayBottomLine
+
+    let displayTopNGames (limit : int) = displayResults (sprintf "Top %d Games" limit, limit)
+    let displayTop25Games = 25 |> displayTopNGames
+
+    let displayNGamesToPlay (limit : int) = displayResults(sprintf "Top %d Games to Play" limit, limit)
+    let display15GamesToPlay = 15 |> displayNGamesToPlay
+
+    let displayNGamesToSellOrTrade (limit : int) = displayResults (sprintf "Top %d Games to Sell / Trade" limit, limit)
+    let display15GamesToSellOrTrade = 15 |> displayNGamesToSellOrTrade
--- src/Misc.BgStats/Program.fs
+++ src/Misc.BgStats/Program.fs
@@ -37,69 +37,6 @@ module Program =
         client.BaseAddress <- config.BoardGameGeek.Url
         client
 
-    let getScore (scoreType : ScoreType) (scores : Score list) =
-        (scores |> List.find (fun s -> s.ScoreType = scoreType)).Value
-
-    let displayResults (title : string, limit : int) (rankings : Ranking list) =
-        printf "%s%s%s" Environment.NewLine title Environment.NewLine
-
-        let topN = rankings |> List.take (limit)
-
-        let longestName = 
-            topN 
-            |> List.map (fun r -> r.BoardGame.Name.Length)
-            |> List.max
-
-        printf "\u2554"
-        [0..3] |> List.iter (fun _ -> printf "\u2550")
-        printf "\u2564"
-        [0..longestName+1] |> List.iter (fun _ -> printf "\u2550")
-        printf "\u2564"
-        [0..7] |> List.iter (fun _ -> printf "\u2550")
-        printf "\u2564"
-        [0..7] |> List.iter (fun _ -> printf "\u2550")
-        printf "\u2564"
-        [0..7] |> List.iter (fun _ -> printf "\u2550")
-        printf "\u2564"
-        [0..7] |> List.iter (fun _ -> printf "\u2550")
-        printfn "\u2557"
-
-        topN
-        |> List.iter (fun r ->
-            printfn 
-                "\u2551%3d \u2502 %-*s \u2502 %6.2f \u2502 %6.2f \u2502 %6.2f \u2502 %6.2f \u2551"
-                r.Position
-                longestName
-                r.BoardGame.Name
-                //((char 179).ToString())
-                (r.Evaluation.Scores |> List.sumBy (fun s -> s.Value))
-                (r.Evaluation.Scores |> getScore ScoreType.Rating)
-                (r.Evaluation.Scores |> getScore ScoreType.Plays)
-                (r.Evaluation.Scores |> getScore ScoreType.Ownership))
-
-        printf "\u255A"
-        [0..3] |> List.iter (fun _ -> printf "\u2550")
-        printf "\u2567"
-        [0..longestName+1] |> List.iter (fun _ -> printf "\u2550")
-        printf "\u2567"
-        [0..7] |> List.iter (fun _ -> printf "\u2550")
-        printf "\u2567"
-        [0..7] |> List.iter (fun _ -> printf "\u2550")
-        printf "\u2567"
-        [0..7] |> List.iter (fun _ -> printf "\u2550")
-        printf "\u2567"
-        [0..7] |> List.iter (fun _ -> printf "\u2550")
-        printfn "\u255D"
-
-    let displayTopNGames (limit : int) = displayResults (sprintf "Top %d Games" limit, limit)
-    let displayTop25Games = 25 |> displayTopNGames
-
-    let displayNGamesToPlay (limit : int) = displayResults(sprintf "Top %d Games to Play" limit, limit)
-    let display15GamesToPlay = 15 |> displayNGamesToPlay
-
-    let displayNGamesToSellOrTrade (limit : int) = displayResults (sprintf "Top %d Games to Sell / Trade" limit, limit)
-    let display15GamesToSellOrTrade = 15 |> displayNGamesToSellOrTrade
-
     let runAsync = 
         task {
             use client = initClient()
@@ -107,9 +44,9 @@ module Program =
             let! collection = client |> BoardGameGeekClient.getCollectionAsync config.BoardGameGeek logger
             let evaluations = collection |> Evaluator.evaluate
 
-            evaluations |> Ranker.Top25AllTime |> displayTop25Games
-            evaluations |> Ranker.GamesToPlay |> display15GamesToPlay
-            evaluations |> Ranker.GamesToSellOrTrade |> display15GamesToSellOrTrade
+            evaluations |> Ranker.Top25AllTime |> Renderer.displayTop25Games
+            evaluations |> Ranker.GamesToPlay |> Renderer.display15GamesToPlay
+            evaluations |> Ranker.GamesToSellOrTrade |> Renderer.display15GamesToSellOrTrade
 
             printf "%sPress ENTER to exit: " Environment.NewLine
             Console.ReadLine() |> ignore
