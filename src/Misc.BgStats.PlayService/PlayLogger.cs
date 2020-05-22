d6ac9ffb404f5ffc79644eec20bc6e6b0419aa3e
Michael J Lindegarde <mlindegarde@e2g.com>
2020-05-22 09:18:37 -0400

    Added project.

---
+++ src/Misc.BgStats.PlayService/PlayLogger.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Topshelf.Runtime;
+
+namespace Misc.BgStats.PlayService
+{
+    public class PlayLogger
+    {
+        #region Member Variables
+        private readonly HostSettings _hostSettings;
+
+        private Task _serviceTask;
+        private CancellationTokenSource _tokenSource = new CancellationTokenSource();
+        private CancellationToken _cancellationToken;
+        #endregion
+
+        #region Constructor
+        public PlayLogger(HostSettings hostSettings)
+        {
+            _hostSettings = hostSettings;
+        }
+        #endregion
+
+        #region Service Management
+        public void Start()
+        {
+            _cancellationToken = _tokenSource.Token;
+            _serviceTask = StartAsync();
+        }
+
+        public async Task StartAsync()
+        {
+            await RunAsync();
+        }
+
+        public void Stop()
+        {
+            _tokenSource.Cancel();
+
+            if(_serviceTask?.IsCompleted == false)
+                _serviceTask.Wait();
+        }
+        #endregion
+
+        #region Main Execution Loop
+        private async Task RunAsync()
+        {
+            while (!_cancellationToken.IsCancellationRequested)
+            {
+
+
+
+                await Task.Delay(TimeSpan.FromMinutes(30), _cancellationToken);
+            }
+        }
+        #endregion
+    }
+}
---
+++ src/Misc.BgStats.PlayService/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using Topshelf;
+
+namespace Misc.BgStats.PlayService
+{
+    public class Program
+    {
+        #region Constants
+        private const string ServiceName = "BgStats.PlayService";
+        private const string ServiceDisplayName = "BG Stats Play Service";
+        private const string ServiceDescription = "Service used to copy BGG plays locally";
+        #endregion
+
+        #region Member Variables
+        private TopshelfExitCode _exitCode;
+        #endregion
+
+        #region Start-up / Config
+        private Program LoadConfig()
+        {
+            return this;
+        }
+
+        private Program InitLogger()
+        {
+            return this;
+        }
+
+        private Program InitIoC()
+        {
+            return this;
+        }
+        #endregion
+
+        #region Main Method
+        private Program Run()
+        {
+            _exitCode =
+                HostFactory.Run(
+                    hostConfig =>
+                    {
+                        hostConfig.Service<PlayLogger>(
+                            serviceConfig =>
+                            {
+                                serviceConfig.ConstructUsing(hostSettings => new PlayLogger(hostSettings));
+                                serviceConfig.WhenStarted(x => x.Start());
+                                serviceConfig.WhenStopped(x => x.Stop());
+                            });
+                        hostConfig.RunAsLocalSystem();
+
+                        hostConfig.SetServiceName(ServiceName);
+                        hostConfig.SetDisplayName(ServiceDisplayName);
+                        hostConfig.SetDescription(ServiceDescription);
+                    });
+
+            return this;
+        }
+        #endregion
+
+        #region Shutdown / Clean-up
+        private void Shutdown()
+        {
+            Environment.ExitCode = (int)_exitCode;
+        }
+        #endregion
+
+        #region Entrypoint
+        static void Main()
+        {
+            Program program = new Program();
+
+            program
+                .LoadConfig()
+                .InitLogger()
+                .InitIoC()
+                .Run()
+                .Shutdown();
+        }
+        #endregion
+    }
+}
